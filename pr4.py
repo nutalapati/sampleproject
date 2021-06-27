183c2cbc6b95f5d280920287b5e569b21dec21ae
nutalapati <nutalapati22@gmail.com>
2021-06-27 12:26:07 +0530

    file four added

---
+++ pr4.py
@@ -0,0 +1,13 @@
+# program to demonstrate input and output 
+pin,cash = input('enter your pin and cash:').split('*')
+print ('pin and cash are ', pin,cash)
+cashAmount=int(cash)
+#print('type of cash is :',type(cashAmount))
+atmValue = 50000
+remainingAmount = atmValue - cashAmount
+#print ('take your cash',cashAmount ,'and remaining balance is ', remainingAmount )
+print ('take your cash {1}, remaining balance in your account {0}'.format(cashAmount,remainingAmount))
+print ('take your cash 1000', cash ,remainingAmount,sep="->", end="======")
+print ('thank you visit again')
+print ('thank you suresh techs for great teaching ')
+
